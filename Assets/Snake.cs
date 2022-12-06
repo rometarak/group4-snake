a98d5a6a24f98b39dd28161c25be3d1a0512a544
Romet Arak <romet.arak@gmail.com>
2022-12-07 00:09:00 +0200

    Lisatud toidu süsteem ja snakei pikkuse muutus, kui tekib collision toiduga

--- Assets/Snake.cs
+++ Assets/Snake.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    private Vector2 _direction = Vector2.right;
+    private Vector2 _direction = Vector2.zero;
+    private List<Transform> snakeSegments;
+    public Transform snakeSegmentPrefab;
+
+    private void Start(){
+        snakeSegments = new List<Transform>();
+        snakeSegments.Add(this.transform);
+    }
 
     private void Update(){
         
@@ -24,10 +31,27 @@ public class Snake : MonoBehaviour
     }
 
     private void FixedUpdate(){
+        for(int i = snakeSegments.Count - 1; i > 0; i--){
+            snakeSegments[i].position = snakeSegments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + _direction.x,
             Mathf.Round(this.transform.position.y) + _direction.y,
             0.0f
         );
+    }
+
+    private void Grow(){
+        Transform segment = Instantiate(this.snakeSegmentPrefab);
+        segment.position = snakeSegments[snakeSegments.Count - 1].position;
+        snakeSegments.Add(segment);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D snake){
+        if(snake.tag == "Food"){
+            Grow();
+        }
+        
     }
 }
